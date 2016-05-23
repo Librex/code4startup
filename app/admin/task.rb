d3c52927c01f73f0e4503c2b05e2678eed411c98
ychino <a2106349@gmail.com>
2016-05-23 21:48:53 +0000

    commit after admin corrected

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -1,34 +1,34 @@
 ActiveAdmin.register Task do
-	permit_params :title, :note, :video, :header, :tag, :project_id
+  permit_params :title, :note, :video, :header, :tag, :project_id
 
-	sortable tree: false,
-						sorting_attribute: :tag
+  sortable tree: false,
+           sorting_attribute: :tag
 
-	index :as => :sortable do
-		label :title
+  index as: :sortable do
+    label :title
 
-		actions
-	end
+    actions
+  end
 
-	index do 
-		selectable_column
-		column :header
-		column :title
-		column :tag
-		column :project
+  index do
+    selectable_column
+    column :header
+    column :title
+    column :tag
+    column :project
 
-		actions
-	end
-	
-	form do |f|
-		f.inputs do
-			input :project, label: "Project"
-			input :title, label: "Title"
-			input :note, label: "Note"
-			input :video, label: "video"
-			input :header, label: "Header"
-		end
+    actions
+  end
 
-		actions
-	end
+  form do |f|
+    f.inputs do
+      input :project, label: 'Project'
+      input :title, label: 'Title'
+      input :note, label: 'Note'
+      input :video, label: 'video'
+      input :header, label: 'Header'
+    end
+
+    actions
+  end
 end
