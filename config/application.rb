cc874ac74e672df28b86094859262de48bdfc6c8
seiyamiyaoka <seiyamiyaoka@gmail.com>
2016-07-18 13:14:27 +0900

    webconsole edit

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Code4startup
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.web_console.development_only = false
+    config.web_console.development_only = true
     config.active_record.raise_in_transactional_callbacks = true
     config.generators do |g|
       g.helper false
