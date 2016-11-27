f9fe1c8472630a6aaa14a35493a694ecccc21f88
seiyamiyaoka <seiyamiyaoka@gmail.com>
2016-11-27 15:35:00 +0900

    stagingキー設定

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-role :app, %w{staging-test.code4startup.jp}
-role :web, %w{staging-test.code4startup.jp}
-role :db,  %w{staging-test.code4startup.jp}
+role :app, %w{app@staging-test.code4startup.jp}
+role :web, %w{app@staging-test.code4startup.jp}
+role :db,  %w{app@staging-test.code4startup.jp}
