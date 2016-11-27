781ebde3ea377d1d55c6416a9daaf59ec661a9c9
seiyamiyaoka <seiyamiyaoka@gmail.com>
2016-11-27 15:24:07 +0900

    staging用ファイル作成

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
-role :app, %w{app@staging.code4startup.jp}
-role :web, %w{app@staging.code4startup.jp}
-role :db,  %w{app@staging.code4startup.jp}
+role :app, %w{staging-test.code4startup.jp}
+role :web, %w{staging-test.code4startup.jp}
+role :db,  %w{staging-test.code4startup.jp}
